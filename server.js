ba741c1ee97da79e1c8b3147ca818dd9a06d0532
Susanne Heim <s-heim@mailbox.org>
2023-04-15 10:42:35 +0200

    setup server to store entries

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
-projectData = {};
+projectData = [];
 
-// Initialize Express Application with Middleware
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
+const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 app.use(express.static('website'));
 
-// Setup Server
-const port = 8000;
-const server = app.listen(port, () => console.log(`server running on localhost:${port}`));
+app.listen(port, () => console.log(`server running on localhost:${port}`));
+
+app.get("/all", (req,res) => {
+    res.send(projectData);
+});
+
+app.post("/entry", (req,res) => {
+    projectData.push(req.body);
+});
--- website/app.js
+++ website/app.js
@@ -1,22 +1,39 @@
 // API Helper
 
-const baseURL = "https://api.openweathermap.org";
+const apiURL = "https://api.openweathermap.org";
 const apiKey = "<API-KEY>";
 
 /**
- * Creates a HTTP Requests
+ * Creates a HTTP-GET-Request to a specified api
  * @param {string}  baseURL     The main url of the api
- * @param {string}  subURL      The defining suburl of the api service
+ * @param {string}  route       The defining route of the api service
  * @param {string}  params      The request parameters
  * @param {string}  key         The api key
  * @returns An unhandled HTTP request (serialized from JSON)
  */
-const request = async (baseURL, subURL, params, appendix) => {
-    return await fetch(baseURL + subURL + params + "&appid=" + appendix).then(res => res.json());
+const get = async (baseURL, route, params, appendix) => {
+    return await fetch(baseURL + route + params + "&appid=" + appendix).then(res => res.json());
 }
 
 /**
- * Creates a HTTP Request to the geo api
+ * Creates a HTTP-POST-Request to the server
+ * @param {string}  route       The route to the post service
+ * @param {Object}  data        The data to post
+ * @returns An unhandled HTTP request
+ */
+const post = async (route, data) => {
+    return await fetch(route, {
+        method: 'POST', 
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+        },     
+        body: JSON.stringify(data), 
+      });
+}
+
+/**
+ * Creates a HTTP-GET-Request to the geo api
  * @param {string}  city        The city name of the requested location
  * @param {string}  country     The country code of the requested location
  * @returns An unhandled HTTP request (serialized from JSON)
@@ -26,11 +43,11 @@ const getGeoData = async (city, country) => {
         return undefined;
     }
 
-    return request(baseURL, "/geo/1.0/direct?", `q=${city ? city : ""}${country && city ? "," : ""}${country ? country : ""}`, apiKey);
+    return get(apiURL, "/geo/1.0/direct?", `q=${city ? city : ""}${country && city ? "," : ""}${country ? country : ""}`, apiKey);
 }
 
 /**
- * Creates a HTTP request to the weather api
+ * Creates a HTTP-GET-request to the weather api
  * @param {number}  lat     The latitude of the location
  * @param {number}  lon     The longitude of the location
  * @returns An unhandled HTTP request (serialized from JSON)
@@ -40,7 +57,16 @@ const getWeatherData = async (lat, lon) => {
         return undefined;
     }
 
-    return request(baseURL, "/data/2.5/weather?", `lat=${lat}&lon=${lon}&units=metric`, apiKey);
+    return get(apiURL, "/data/2.5/weather?", `lat=${lat}&lon=${lon}&units=metric`, apiKey);
+}
+
+/**
+ * Creates a HTTP-POST-Request to the Entry Handler
+ * @param {Object}  data    The Entry Data
+ * @returns An unhandled HTTP request
+ */
+const postEntryData = async (data) => {
+    return post("/entry", data);
 }
 
 
@@ -48,14 +74,12 @@ const getWeatherData = async (lat, lon) => {
 
 /**
  * Sets the data to the most recent entry
- * @param {Date}    date    The date of the entry
- * @param {number}  temp    The temperature
- * @param {string}  content The feelings (or other content to mention)
+ * @param {Objct}   data    The entry data
  */
-const setEntryData = (date = new Date(), temp=null, content=null) => {
-    document.getElementById("date").innerHTML = date.toUTCString();
-    document.getElementById("temp").innerHTML = temp;
-    document.getElementById("content").innerHTML = content;
+const setEntryData = (data) => {
+    document.getElementById("date").innerHTML = data.date.toUTCString();
+    document.getElementById("temp").innerHTML = data.temp ? data.temp : "";
+    document.getElementById("content").innerHTML = data.content ? data.content : "";
 } 
 
 
@@ -72,7 +96,16 @@ document.getElementById("generate").addEventListener("click", () => {
             if (res.length == 1) {
                 const { lat, lon } = res[0];
                 getWeatherData(lat, lon).then(res => {
-                    setEntryData(new Date(), res.main.temp + "°C", "Your feelings: " + feelings.value);
+                    const data = { 
+                        date: new Date(), 
+                        temp: res.main.temp + "°C", 
+                        content: "Your feelings: " + feelings.value 
+                    };
+
+                    setEntryData(data   );
+                    postEntryData(data).then(res => console.log(res)).catch(err => console.log(err));
+                    
+
                 }).catch(err => console.log(err));
             }
             
